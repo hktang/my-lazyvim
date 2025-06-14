5d42ea433c823ef98bf0ba771e0a15b9c15b2046
Xiaojun Deng <hiukuan.tang@gmail.com>
2025-06-14 22:07:34 +0900

    Update formatting options.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+    },
+  },
+}
